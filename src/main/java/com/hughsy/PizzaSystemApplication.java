25bd0d748ba5caa5ffe71d1ebd4ee24fdf8ffbbc
hughsylinden <kaarelinden@hotmail.com>
2022-03-20 15:04:31 +0000

    added entities and fixed bugs preventing server from starting

---
+++ src/main/java/com/hughsy/PizzaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hughsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hughsy/PizzaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hughsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
